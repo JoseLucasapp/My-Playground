804bb7315037cb96e3b6c3965c2b982146b2299e
José Lucas <58341307+JoseLucasapp@users.noreply.github.com>
2020-10-04 15:23:01 -0300

    Update PasswordGenerator.cs

--- PasswordGenerator.cs
+++ PasswordGenerator.cs
@@ -4,15 +4,19 @@ class MainClass {
   public static void Main (string[] args){
     Console.WriteLine("Welcome to Password Generator:");
     Console.WriteLine("How many characters does your password need?");
+
     Random randNum = new Random();
+
     int password_length;
     int length = Convert.ToInt32(Console.ReadLine());
     string max = "10";
+
     for(int i = 1; i < length; i++){
       max += "0";
     }
+
     password_length = Convert.ToInt32(max);
-    int c = randNum.Next(password_length);
-    Console.WriteLine(c);
+    int password = randNum.Next(password_length);
+    Console.WriteLine(password);
   }
 }
