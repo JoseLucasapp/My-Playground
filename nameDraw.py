ca2cf32ee3ff7ef12e9a6b18044e883411f19b6b
José Lucas <58341307+JoseLucasapp@users.noreply.github.com>
2020-10-20 19:39:17 -0300

    Update nameDraw.py

--- nameDraw.py
+++ nameDraw.py
@@ -1,22 +1,38 @@
 from random import randint
-maximo = int(input('Quantas pessoas vão participar do sorteio ? '))
-sorteadas = int(input('Quantas pessoas serão sorteadas ?'))
 
 pessoas = []
 numsorteados = []
+continuar = 'S'
+n = ''
+while True:
+  print('')
+  print(f'Iniciando {n}sorteio')
+  print('')
+  maximo = int(input('Quantas pessoas vão participar do sorteio ? '))
+  sorteadas = int(input('Quantas pessoas serão sorteadas ?'))
 
-for i in range(0,maximo):
-  print(f'informe o nome da {i+1}° pessoa ')
-  pessoa = input('-> ')
-  pessoas.append(pessoa)
+  for i in range(0,maximo):
+    print(f'informe o nome da {i+1}° pessoa ')
+    pessoa = input('-> ')
+    print('')
+    pessoas.append(pessoa)
 
-for a in range(0,sorteadas):
-  sorteio = randint(0,(maximo - 1))
-  while True :
-    try:
-      numsorteados.index(sorteio)
-      sorteio = randint(0,(maximo - 1))
-    except:
-      break
-  numsorteados.append(sorteio)
-  print(f'A {a+1}° pessoa sorteada foi {pessoas[sorteio]}')
+  for a in range(0,sorteadas):
+    sorteio = randint(0,(maximo - 1))
+    while True :
+      try:
+        numsorteados.index(sorteio)
+        sorteio = randint(0,(maximo - 1))
+      except:
+        break
+    numsorteados.append(sorteio)
+    if sorteadas > 1:
+      print(f'A {a+1}° pessoa sorteada foi {pessoas[sorteio]}')
+    else:
+      print(f'A pessoa sorteada foi {pessoas[sorteio]}')
+    print('')
+  continuar = input('deseja sortear novamente? (S/N)').upper()
+  if continuar == 'N' or continuar =='NAO' or continuar == 'NÃO':
+    break
+  else:
+    n = 'novo '
